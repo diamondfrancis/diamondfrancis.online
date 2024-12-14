77320c40494b65162ee2e986926cf8e3a4f6c600
Diamond Francis <hello@diamondfrancis.co.uk>
2024-12-14 23:18:08 +0000

    site update

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileToggle = document.querySelector('.mobile-toggle');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const closeMenu = document.querySelector('.close-menu');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
+  // Toggle Mobile Menu
+  mobileToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+
+  // Close Mobile Menu
+  closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
 });
